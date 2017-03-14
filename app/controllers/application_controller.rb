c1444095b9e3fb318154724875ea4304d5481945
bcwik <bcwik9@gmail.com>
2017-03-14 18:28:53 +0000

    fix debug stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_filter :check_debug
-
   def user_logged_in_to_robinhood?
     session[:robinhood_auth_token].present?
-  end
-
-  def check_debug
-    @debug = true if params[:debug].present?
   end
 end
