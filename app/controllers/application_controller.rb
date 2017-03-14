d3064c224956a98179d581f138b29a596d04e47a
bcwik <bcwik9@gmail.com>
2017-03-14 13:49:45 +0000

    add debug flag, fix a few bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_filter :check_debug
+
   def user_logged_in_to_robinhood?
     session[:robinhood_auth_token].present?
+  end
+
+  def check_debug
+    @debug = true if params[:debug].present?
   end
 end
