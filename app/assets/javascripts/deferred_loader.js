5ffc00728d9cb72840660559c76e28af555ec221
bcwik <bcwik9@gmail.com>
2017-03-17 20:05:54 +0000

    add refresh functionality

--- app/assets/javascripts/deferred_loader.js
+++ app/assets/javascripts/deferred_loader.js
@@ -1,8 +1,16 @@
 DeferredLoader = function(element) {
-  if(element.html() == "" && !element.data("loaded")) {
-    element.data("loaded", true)
+
+  var update_content = function() {
     $.get(element.data("path"), function(data) {
-      element.html(data)
+      element.html(data);
     })
+  }
+
+  if(element.html() == "" && !element.data("loaded")) {
+    element.data("loaded", true);
+    update_content();
+    if(element.data("refresh")){
+      setInterval(function(){ update_content(); }, element.data("refresh")*1000);
+    }
   }
 }
