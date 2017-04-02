79922cb7e26e44f02fb37843fd4d5561add9bc47
bcwik <bcwik9@gmail.com>
2017-04-02 20:09:03 +0000

    remove extra sidebar. fix affix bug

--- app/assets/javascripts/affix.js
+++ app/assets/javascripts/affix.js
@@ -1,3 +1,9 @@
 $(function() {
   $(".affix").affix()
+  $(document).on("affixed-top.bs.affix", function(event) {
+    $(".side-menu").removeClass("affix-top")
+    $(".side-menu").addClass("affix")
+  })
+  $(".side-menu").removeClass("affix-top")
+  $(".side-menu").addClass("affix")
 })
