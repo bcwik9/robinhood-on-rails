e6545a6cade61a79e0a0fbc7adc275c53dd2d13d
bcwik <bcwik9@gmail.com>
2017-04-07 18:10:59 +0000

    fix a bug with charts

--- app/assets/javascripts/fundamentals_tooltip.js
+++ app/assets/javascripts/fundamentals_tooltip.js
@@ -1,24 +1,34 @@
 FundamentalsTooltip = function(element) {
 
+  var symbols = $(".symbol")
+
   var hide_all = function() {
-    $(".symbol").tooltip("hide")
+    symbols.tooltip("hide")
   }
 
-  $(".symbol").hover(function(e) {
-    var target = $(e.target)
-    if(!target.data("tooltip-loaded")){
-      target.data("tooltip-loaded", true)
-      var url = "fundamentals/?symbols=" + e.target.text
-      $.get(url, function(data){
-        target.attr("title", data)
-        target.tooltip("show")
-        Elemental.load(target.siblings(".tooltip"))
-      })
-    }
-    hide_all()
-  })
 
-  $(document).click(function() {
-    hide_all()
-  })
+  var init = function() {
+    symbols.on("shown.bs.tooltip", function(e) {
+      Elemental.load($(e.target).siblings(".tooltip"))
+    })
+
+    symbols.hover(function(e) {
+      hide_all()
+      var target = $(e.target)
+      if(!target.data("tooltip-loaded")){
+        target.data("tooltip-loaded", true)
+        var url = "fundamentals/?symbols=" + e.target.text
+        $.get(url, function(data){
+          target.attr("title", data)
+          target.tooltip("show")
+        })
+      }
+    })
+
+    $(document).click(function() {
+      hide_all()
+    })
+  }
+
+  init()
 }
