184581ad73997273048949a6bd7535ab2ff595e3
bcwik <bcwik9@gmail.com>
2017-04-08 03:33:57 +0000

    some styling love for the submenu

--- app/assets/javascripts/submenu.js
+++ app/assets/javascripts/submenu.js
@@ -1,17 +1,24 @@
 SubMenu = function(element) {
   var tabs = element.find("li")
+  var all_tab_content = $(".sub-menu-tab")
+
+  var set_initial_state = function() {
+    all_tab_content.hide()
+    $(all_tab_content[0]).show()
+  }
 
   var init = function() {
     tabs.click(function(event) {
       event.preventDefault()
       tab = $(event.target)
-      all_tab_content = $(".sub-menu-tab")
       all_tab_content.hide()
       tabs.removeClass("active")
       target_content = $(tab.data("target"))
       tab.parent().addClass("active")
       target_content.show()
     })
+
+    set_initial_state()
   }
 
   init()
