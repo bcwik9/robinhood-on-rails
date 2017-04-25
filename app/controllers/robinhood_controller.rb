70f8c2a8d052019aa1b5ea72417dfd6b433f78ce
bcwik <bcwik9@gmail.com>
2017-04-25 23:03:03 +0000

    fix some bugs surrounding partially filled orders

--- app/controllers/robinhood_controller.rb
+++ app/controllers/robinhood_controller.rb
@@ -315,6 +315,7 @@ class RobinhoodController < ApplicationController
     get_orders
     @orders.each do |order|
       order["instrument"] = robinhood_get order["instrument"]
+      order["filled_quantity"] = order["executions"].map{|e| e["quantity"].to_i}.sum.to_s
     end
   end
 
