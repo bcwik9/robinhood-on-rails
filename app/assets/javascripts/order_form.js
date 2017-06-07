5880bf099d081b032311c1a2784633abb1e4f756
bcwik <bcwik9@gmail.com>
2017-06-07 18:20:24 +0000

    add some additional info around stop losses

--- app/assets/javascripts/order_form.js
+++ app/assets/javascripts/order_form.js
@@ -13,7 +13,7 @@ OrderForm = function(element) {
       } else if(type === "Limit") {
         return "Limit orders specify the maximum amount you are willing to pay for a stock."
       } else if(type === "Stop loss") {
-        return "Stop loss orders trigger a market order to buy when the stop price is reached."
+        return "Stop loss orders trigger a market order to buy when the stop price is reached. Enter a price higher than the current price."
       } else if(type === "Stop limit") {
         return "Stop limit orders will trigger a specified limit order when the stop price is met. This may be used to limit the price your trade will execute for after the stop is triggered, but there is risk that it will not execute if the stock moves past it."
       }
@@ -24,7 +24,7 @@ OrderForm = function(element) {
       } else if(type === "Limit") {
         return " Limit orders specify the minimum amount you are willing to receive when selling a stock."
       } else if(type === "Stop loss") {
-        return "Stop loss orders trigger a market order to sell when the stop price is met."
+        return "Stop loss orders trigger a market order to sell when the stop price is met. Enter a price below the current price."
       } else if(type === "Stop limit") {
         return "Stop limit orders will trigger a specified limit order when the stop price is reached. This may be used to limit the price your trade will execute for after the stop is triggered, but there is risk that it will not execute if the stock moves past it."
       }
